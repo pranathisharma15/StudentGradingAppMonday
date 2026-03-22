807dc2d790d7ff0f366bfa8ffa40b07110a26cb7
Pranathi Sharma <pranathi.sharma@nexergroup.com>
2026-03-22 18:31:55 +0530

    Initial commit

---
+++ StudentApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of students: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("\nStudent " + (i + 1));
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Marks: ");
+            int marks = int.Parse(Console.ReadLine());
+
+            Student s = new Student(name, age, marks);
+            s.Display();
+
+        }
+    }
+}
---
+++ StudentApp/Student.cs
@@ -0,0 +1,32 @@
+﻿class Student
+{
+    public string Name;
+    public int Age;
+    public int Marks;
+
+    public Student(string name, int age, int marks)
+    {
+        Name = name;
+        Age = age;
+        Marks = marks;
+    }
+
+    public string CalculateGrade()
+    {
+        return Marks switch
+        {
+            >= 90 => "A",
+            >= 75 => "B",
+            >= 50 => "C",
+            _ => "Fail"
+        };
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Marks: " + Marks);
+        Console.WriteLine("Grade: " + CalculateGrade());
+    }
+}
